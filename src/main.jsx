be6bd7f6d91a4f8eee8c465a69b2c3dfcd75b942
tair64 <137691002+tair64@users.noreply.github.com>
2026-02-23 11:54:27 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
-import React from 'react' import ReactDOM from 'react-dom/client' import MealOrderApp from './meal-order-app.jsx' ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <MealOrderApp /> </React.StrictMode> )
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MealOrderApp from './meal-order-app.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MealOrderApp />
+  </React.StrictMode>
+)
