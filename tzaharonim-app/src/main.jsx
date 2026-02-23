e54abfc0c76485f8d38c499e22dca7575ded9906
tair64 <137691002+tair64@users.noreply.github.com>
2026-02-23 07:49:43 +0200

    Add files via upload

---
+++ tzaharonim-app/src/main.jsx
@@ -0,0 +1,1 @@
+import React from 'react' import ReactDOM from 'react-dom/client' import MealOrderApp from './meal-order-app.jsx' ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <MealOrderApp /> </React.StrictMode> )
